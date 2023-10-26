9735f76ff9002e1b9ee69f4e9e88f53ba9b738e9
Nisfan <mohnisfan@gmail.com>
2023-10-26 18:06:25 +0530

    fix pusher

--- app/serverActions.ts
+++ app/serverActions.ts
@@ -15,40 +15,39 @@ const pusher = new Pusher({
   useTLS: true,
 });
 
-stream.on("event1", async () => {
-  await sleep(2000);
+// stream.on("event1", async () => {
+//   await sleep(2000);
+//
+//   // emitEvent("event2", {
+//   //   id: "123",
+//   // });
+//
+// });
 
-  // emitEvent("event2", {
-  //   id: "123",
-  // });
-
-  pusher.trigger("my-channel", "my-event", {
-    message: "hello world",
-  });
-});
-
-async function emitEvent(eventType: string, data: Record<string, any> | null) {
-  const body = {
-    sessionId: "session1",
-    event: eventType,
-    data,
-  };
-  const request = await fetch(`${process.env.NEXT_PUBLIC_SSE_URL}/sse`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(body),
-  });
-
-  return request.json();
-}
+// async function emitEvent(eventType: string, data: Record<string, any> | null) {
+//   const body = {
+//     sessionId: "session1",
+//     event: eventType,
+//     data,
+//   };
+//   const request = await fetch(`${process.env.NEXT_PUBLIC_SSE_URL}/sse`, {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify(body),
+//   });
+//
+//   return request.json();
+// }
 
 export async function addToCart(prevState: any, _: FormData) {
   //This console.log printed twice by clicking `Add to cart` button
 
-  await sleep(2000);
-  stream.emit("event1");
+  await pusher.trigger("my-channel", "my-event", {
+    message: "hello world",
+  });
+  // stream.emit("event1");
 
   return {
     error: null,
